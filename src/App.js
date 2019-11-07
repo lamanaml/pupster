15f0d54faf31dfe7e6fd2a76254268a05b102d13
lamana <lamanaml@gmail.com>
2019-11-07 15:24:38 -0500

    added package

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./components/pages/About";
 import Discover from "./components/pages/Discover";
 import Search from "./components/pages/Search";
+// import Navbar from "./components/pages/Navbar";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/About" component={About} />
         <Route exact path="/Discover" component={Discover} />
         <Route path="/Search" component={Search} />
+        {/* <Route path="/Navbar" component={Navbar} /> */}
       </div>
     </Router>
   );
---
+++ src/components/pages/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar(props) {
+  return (
+ <div>
+     links will go here
+     
+    </div>
+    
+
+    );
+}
+
+export default Navbar;
