a5f3c0cc8f6bf58213d7d63b901f44e2ef78bf56
lamana <lamanaml@gmail.com>
2019-11-07 16:47:54 -0500

    added components and pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "./components/pages/About";
-import Discover from "./components/pages/Discover";
-import Search from "./components/pages/Search";
-// import Navbar from "./components/pages/Navbar";
+import About from "./pages/About";
+import Discover from "./pages/Discover";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
+ 
     <Router>
-      <div>
+         <div>
+        <Navbar />
         <Route exact path="/" component={About} />
         <Route exact path="/About" component={About} />
         <Route exact path="/Discover" component={Discover} />
         <Route path="/Search" component={Search} />
-        {/* <Route path="/Navbar" component={Navbar} /> */}
+        <Route path="/Navbar" component={Navbar} />
       </div>
     </Router>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function Navbar() {
+  return (
+ <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link to="/about" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          Pupster
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/about"
+          className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
+        >
+          About
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/discover"
+          className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
+        >
+          Discover
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/search"
+          className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+        >
+          Search
+        </Link>
+      </li>
+      
+    </ul>
+    
+
+    );
+}
+
+export default Navbar;
